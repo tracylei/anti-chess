67f80bed686cf2c3b173fece8c18bde78011a042
Tracy Lei <tracylei.yq@gmail.com>
2016-12-03 00:45:06 -0500

    Allow any move if king is in check

--- pychess-0.12.4/lib/pychess/Utils/lutils/lsearch.py
+++ pychess-0.12.4/lib/pychess/Utils/lutils/lsearch.py
@@ -185,10 +185,14 @@ def alphaBeta(board, depth, alpha=-MATE_VALUE, beta=MATE_VALUE, ply=0):
                      if not kingExplode(board, m, board.color)]
         moves = [(-getMoveValue(board, table, depth, m), m) for m in mlist]
     else:
-        mlist = [m for m in genCaptures(board)]
-        if not mlist and not isCheck:
-            mlist = [m for m in genAllMoves(board)]
-        moves = [(-getMoveValue(board, table, depth, m), m) for m in mlist]
+        if isCheck:
+             moves = [(-getMoveValue(board, table, depth, m), m)
+                      for m in genCheckEvasions(board)]
+        else:
+            mlist = [m for m in genCaptures(board)]
+            if not mlist:
+                mlist = [m for m in genAllMoves(board)]
+            moves = [(-getMoveValue(board, table, depth, m), m) for m in mlist]
     moves.sort()
 
     # This is needed on checkmate
